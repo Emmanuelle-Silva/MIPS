5a1921a041be3e5516ac1451645c53aca7b1964f
ismaelg456g <ismael-456@hotmail.com>
2022-04-21 11:58:55 -0300

    sum out port map

--- control_unit.vhd
+++ control_unit.vhd
@@ -68,19 +68,19 @@ component  FSM is
         rst: in std_logic;
         ir: in std_logic_vector (15 downto 0);
         rp_zero:in std_logic;
-        --Dados de saídas para se comunicar com o bloco operacional
+        --Dados de saï¿½das para se comunicar com o bloco operacional
         -- o que tiver o pre-lebal "d_" se comunica fora do bloco operacional
         w_addr, rp_addr, rq_addr: out std_logic_vector (3 downto 0);
         w_wr, rp_rd, rq_rd, d_rd, d_wr: out std_logic;
         d_addr, w_data: out std_logic_vector (7 downto 0);
         rf_s1,rf_s0, ula_s1, ula_s0: out std_logic;
-        --Dados de saídas para se comunicar com  a parte internda da unidade de controle 
+        --Dados de saï¿½das para se comunicar com  a parte internda da unidade de controle 
         ir_ld, i_ld: out std_logic;
         PC_clr, PC_inc, PC_ld: out std_logic
     );
 end component;
 
-signal pc_out_addr, out_IR: std_logic_vector (15 downto 0);
+signal pc_out_addr, out_IR, s_sum_out: std_logic_vector (15 downto 0);
 signal s_ir_ld, s_i_rd, s_pc_ld, s_pc_inc, s_pc_clr: std_logic;
 
 begin
@@ -90,14 +90,14 @@ PC_block: PC port map (
             clr => s_pc_clr,
             clk => clk,
             up  => s_PC_inc,
-            PC_sum => out_IR,
+            PC_sum => s_sum_out,
             PC_out => pc_out_addr
 	       );
 
 sum: somador port map (
 		sum_in => pc_out_addr,
 		IR_in_sum => out_IR, 
-		sum_out => ,
+		sum_out => s_sum_out
 	);
 
 IR_block: inst_reg port map(
@@ -112,7 +112,7 @@ FSM_block: FSM Port map (
         rst => rst,
         ir => out_ir,
         rp_zero => RF_Rp_zero,
-        --Dados de saídas para se comunicar com o bloco operacional
+        --Dados de saï¿½das para se comunicar com o bloco operacional
         -- o que tiver o pre-label "d_" se comunica fora do bloco operacional
         w_addr  => RF_w_addr,
         rp_addr => RF_rp_addr,
@@ -128,7 +128,7 @@ FSM_block: FSM Port map (
         rf_s0   => RF_s0, 
         ula_s1  => alu_s1, 
         ula_s0  => alu_s0,
-        --Dados de saídas para se comunicar com  a parte internda da unidade de controle 
+        --Dados de saï¿½das para se comunicar com  a parte internda da unidade de controle 
         ir_ld   => s_ir_ld,
         i_ld    => s_i_rd,
         PC_clr  => s_pc_clr,
